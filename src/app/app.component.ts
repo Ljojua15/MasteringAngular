1eb1ab79c26d6b2c6b53072b91b414d95a03baf6
Ljojua15 <jojualasha15@gmail.com>
2024-08-30 10:38:41 +0400

    second-part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Lesson1Component } from './lesson-1/lesson-1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule, Lesson1Component],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'AngularCourse';
+  name: string = '';
 }
---
+++ src/app/lesson-1/lesson-1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lesson-1',
+  standalone: true,
+  imports: [],
+  templateUrl: './lesson-1.component.html',
+  styleUrl: './lesson-1.component.scss'
+})
+export class Lesson1Component {
+
+}
