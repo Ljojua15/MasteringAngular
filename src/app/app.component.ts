df4570c3e6d62ec1f1e29a480c6d548e2dac2523
Ljojua15 <jojualasha15@gmail.com>
2024-08-30 10:38:49 +0400

    second-part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,4 @@ import { Lesson1Component } from './lesson-1/lesson-1.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  name: string = '';
-}
+export class AppComponent {}
--- src/app/lesson-1/lesson-1.component.ts
+++ src/app/lesson-1/lesson-1.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-lesson-1',
+  selector: "app-lesson-1",
   standalone: true,
-  imports: [],
-  templateUrl: './lesson-1.component.html',
-  styleUrl: './lesson-1.component.scss'
+  imports: [FormsModule],
+  templateUrl: "./lesson-1.component.html",
+  styleUrl: "./lesson-1.component.scss",
 })
 export class Lesson1Component {
+  name: any = "";
 
+  display(msg: string) {
+    alert(msg);
+  }
+
+  keyPress(e: any) {
+    if (e.shiftKey && e.key === "Y") {
+      console.log(e.key, "sss");
+    }
+  }
 }
