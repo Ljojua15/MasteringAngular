ff476ad0909e316ed67674291f9672d5aeac8080
Ljojua15 <jojualasha15@gmail.com>
2024-09-18 16:58:06 +0400

    'output'

--- src/app/angular-decorators/parameter-decorators/input-output/input-output.component.ts
+++ src/app/angular-decorators/parameter-decorators/input-output/input-output.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from "@angular/common";
+import { CommonModule } from '@angular/common';
 import {
   Component,
   Input,
@@ -6,22 +6,35 @@ import {
   EventEmitter,
   AfterViewChecked,
   AfterViewInit,
-} from "@angular/core";
+} from '@angular/core';
+import { log } from 'node:console';
 
 @Component({
-  selector: "app-input-output",
+  selector: 'app-input-output',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: "./input-output.component.html",
-  styleUrl: "./input-output.component.scss",
+  templateUrl: './input-output.component.html',
+  styleUrl: './input-output.component.scss',
 })
 export class InputOutputComponent {
   @Input() childvalue!: string;
   @Input() childCourses!: { id: number; name: string }[];
+  @Input() isRedPopupOpen!: boolean;
 
+  @Output() close = new EventEmitter<boolean>();
   @Output() text = new EventEmitter();
 
+  hasCloseAnimation: boolean = false;
+
   changeNewText() {
-    this.text.emit("from Child to Patent");
+    this.text.emit('from Child to Patent');
+    console.log('lukaa');
+  }
+
+  closePopup() {
+    this.hasCloseAnimation = true;
+    setTimeout(() => {
+      this.close.emit();
+    }, 2000);
   }
 }
--- src/app/angular-decorators/parameter-decorators/parameter-decorators.component.ts
+++ src/app/angular-decorators/parameter-decorators/parameter-decorators.component.ts
@@ -1,39 +1,47 @@
-import { CommonModule } from "@angular/common";
-import { Component, Input } from "@angular/core";
-import { InputOutputComponent } from "./input-output/input-output.component";
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { InputOutputComponent } from './input-output/input-output.component';
 
 @Component({
-  selector: "app-parameter-decorators",
+  selector: 'app-parameter-decorators',
   standalone: true,
   imports: [CommonModule, InputOutputComponent],
-  templateUrl: "./parameter-decorators.component.html",
-  styleUrl: "./parameter-decorators.component.scss",
+  templateUrl: './parameter-decorators.component.html',
+  styleUrl: './parameter-decorators.component.scss',
 })
 export class ParameterDecoratorsComponent {
-  value: string = "15/may/1998";
+  value: string = '15/may/1998';
 
-  fromChild: string = "";
+  fromChild: string = '';
+
+  isRedPopupOpen: boolean = false;
 
   courses: { id: number; name: string }[] = [
     {
       id: 1,
-      name: "JavaScript Marathon Interview -Questions Series",
+      name: 'JavaScript Marathon Interview -Questions Series',
     },
     {
       id: 2,
-      name: "Mastering React With InterviewQuestions, eStore Project",
+      name: 'Mastering React With InterviewQuestions, eStore Project',
     },
     {
       id: 3,
-      name: "Mastering TypeScript With MarathodInterview Questions",
+      name: 'Mastering TypeScript With MarathodInterview Questions',
     },
     {
       id: 4,
-      name: "Mastering HTML, CSS, Sass, Bootstrap + Interview Questions",
+      name: 'Mastering HTML, CSS, Sass, Bootstrap + Interview Questions',
     },
   ];
 
   recieve(data: string) {
     this.fromChild = data;
+  }
+  openPopup() {
+    this.isRedPopupOpen = true;
+  }
+  onGetE(e: any) {
+    console.log(e);
   }
 }
