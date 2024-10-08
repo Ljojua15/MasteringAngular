720b2239e5a845f1fcd62fb154d4a7ba5d2dd974
Ljojua15 <jojualasha15@gmail.com>
2024-10-08 10:33:35 +0400

    'getpostputdelete'

--- src/app/crud/post/post.component.ts
+++ src/app/crud/post/post.component.ts
@@ -1,22 +1,35 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { log } from 'console';
+import { Observable, tap } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss',
 })
 export class PostComponent {
+  id!: string;
+  name!: string;
+  age!: number;
+  userData$!: Observable<any | undefined>;
   constructor(private http: HttpClient) {}
+
+  getData() {
+    this.userData$ = this.http
+      .get('http://localhost:3000/users')
+      .pipe(tap((res) => console.log('Response:', res)));
+  }
 
   postData() {
     const user = {
-      id: 8,
-      name: 'ABC',
-      age: 100,
+      id: this.id,
+      name: this.name,
+      age: this.age,
     };
     this.http.post('http://localhost:3000/users', user).subscribe((res) => {
       console.log(res);
@@ -25,17 +38,20 @@ export class PostComponent {
 
   putData() {
     const user = {
-      id: '9',
-      name: 'onlynine',
-      age: 21,
+      id: this.id,
+      name: this.name,
+      age: this.age,
     };
-    this.http.put(`http://localhost:3000/users/9`, user).subscribe((res) => {
-      console.log(res);
-    });
+    this.http
+      .put(`http://localhost:3000/users/${user.id}`, user)
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
 
   deleteData() {
-    this.http.delete('http://localhost:3000/users/0').subscribe({
+    const id = this.id;
+    this.http.delete(`http://localhost:3000/users/${id}`).subscribe({
       next: () => {
         console.log('deleted');
       },
